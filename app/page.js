913a829b3a2db7401b4188a5c97dc0b3ed3eca4e
Git 660 <achaldubey023@gmail.com>
2023-09-28 00:08:08 +0530

    use of fat arrow function for paramaterized function

--- app/page.js
+++ app/page.js
@@ -3,14 +3,14 @@ import {React,useState} from 'react'
 
 const page = () => {
   const [name, setname] = useState("JHON")
-  const sumbitHandler=()=>{
-    setname("JHHONVICK")
+  const sumbitHandler=(name)=>{
+    setname(name)
   }
 
   return (
     <div>
 <h1>  {name}  </h1>
-<button onClick={sumbitHandler}> changename</button>
+<button onClick={()=>sumbitHandler("it worked")}> changename</button>
 
     </div>
   )
