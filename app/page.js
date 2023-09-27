705b9c4f6670234996e0015fcb567917cb03b098
Git 660 <achaldubey023@gmail.com>
2023-09-28 01:31:13 +0530

    one way binding

--- app/page.js
+++ app/page.js
@@ -3,14 +3,22 @@ import {React,useState} from 'react'
 
 const page = () => {
   const [name, setname] = useState("JHON")
-  const sumbitHandler=(name)=>{
-    setname(name)
+  const sumbitHandler=(e)=>{
+    e.preventDefault();
+    setname(e.target.username.value)
+
   }
 
   return (
     <div>
+      <form onSubmit={sumbitHandler}>
+<input type="text" placeholder='enter the name ' name='username'/>
+<input type="submit" />
+
+
+      </form>
 <h1>  {name}  </h1>
-<button onClick={()=>sumbitHandler("it worked")}> changename</button>
+{/* <button onClick={()=>sumbitHandler("it worked")}> changename</button> */}
 
     </div>
   )
