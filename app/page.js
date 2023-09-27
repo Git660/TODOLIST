8421671992d0a0edf95ef3376d274f2e19377f46
Git 660 <achaldubey023@gmail.com>
2023-09-28 01:47:47 +0530

    two way binding in next js

--- app/page.js
+++ app/page.js
@@ -3,17 +3,16 @@ import {React,useState} from 'react'
 
 const page = () => {
   const [name, setname] = useState("JHON")
-  const sumbitHandler=(e)=>{
-    e.preventDefault();
-    setname(e.target.username.value)
-
-  }
+  
 
   return (
     <div>
-      <form onSubmit={sumbitHandler}>
-<input type="text" placeholder='enter the name ' name='username'/>
-<input type="submit" />
+      <form>
+<input type="text" placeholder='enter the name 'value={name} name='username' onChange={(e)=>{
+ setname( e.target.value)
+  
+}}/>
+
 
 
       </form>
