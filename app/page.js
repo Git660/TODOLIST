ee8c7faf35e2aa01687352148f7a5745ebd224e9
Git 660 <achaldubey023@gmail.com>
2023-09-28 19:47:44 +0530

    todo list begning

--- app/page.js
+++ app/page.js
@@ -1,25 +1,57 @@
 'use client'
 import {React,useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import  styles  from './page.module.css'
+import { title } from 'process'
 
 const page = () => {
-  const [name, setname] = useState("JHON")
+  const [Title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [state, setstate] = useState("")
+  const createtask=(e)=>{
+    e.preventDefault();
+    const details={
+      Title,desc,state
+    }
   
+
+console.log(details)
+
+
+  }
 
   return (
-    <div>
-      <form>
-<input type="text" placeholder='enter the name 'value={name} name='username' onChange={(e)=>{
- setname( e.target.value)
+    <div >
+    <form   className={styles.frm}   >
+
+<h1>TITLE......</h1>
+    <input type="text" placeholder='title'  onChange={(e)=>{ settitle(e.target.value)}}  style={{resize:"none"}}/>
+    <br />
+    <br />
+    <h1>DESCRIPTION</h1>
+    <textarea    className={styles.txt} style={{resize:"none"}} onChange={(e)=>{  setdesc(e.target.value)}}>
+      
+    </textarea>
+    <br />
+    <br />
+    {/* Remove the defaultValue prop from the select element */}
+    <h1>STATE</h1>
+    <select name="state" id=""  onChange={(e)=>{setstate(e.target.value)}}>
+<option value="Due">Due</option>
+<option value="pending">pending</option>
+<option value="Complete">Complete</option>
+</select>
+
+    <br />
+    <br />
   
-}}/>
-
-
-
-      </form>
-<h1>  {name}  </h1>
-{/* <button onClick={()=>sumbitHandler("it worked")}> changename</button> */}
+    <div className={styles.btns}>
+      <button className={styles.ct} onClick={createtask}>CREATE TASK</button>
+      <button className={styles.ut}>UPDATE TASK</button>
 
     </div>
+  </form>
+  </div>
   )
 }
 
